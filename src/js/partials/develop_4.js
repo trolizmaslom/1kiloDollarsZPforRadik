1eaa38c6b32995764db841db04fbaf64e15dd062
Radiksman <radion91@gmail.com>
2016-01-18 11:42:51 +0200

    фвв іщьу ірше

--- src/js/partials/develop_4.js
+++ src/js/partials/develop_4.js
@@ -1,5 +1,16 @@
-$(document).ready(function(){
+function inputFocus(){
+    $('.form_input input,.form_input textarea').focus(function() {
+        $(this).parent().find('label').addClass('active');
+    });
+    $('.form_input input,.form_input textarea').blur(function() {
+        if(!$(this).val()){
+            $(this).parent().find('label').removeClass('active');
+        }
+    });
+}
 
+$(document).ready(function(){
+    inputFocus();
 });
 
 $(window).load(function(){
