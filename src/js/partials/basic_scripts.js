c4e1f85ec43a1b81dad4ca04b10b16b68bed5fe0
Radiksman <radion91@gmail.com>
2016-01-19 12:41:04 +0200

    add animation

--- src/js/partials/basic_scripts.js
+++ src/js/partials/basic_scripts.js
@@ -107,6 +107,13 @@ function cutText(){
     });
 };
 
+
+/* Scroll Reveal*/
+window.sr = ScrollReveal({ reset: true });
+sr.reveal('.scroll-anim');
+
+
+
 /* DOCUMENT READY  */
 $(document).ready(function() {
     oneHeightItems();
